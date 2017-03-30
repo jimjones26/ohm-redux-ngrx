8b89638a9577d30e30a4c394cd4977fc560d9fb6
jimjones26 <jimjones26@gmail.com>
2017-03-30 18:23:46 -0400

    fill out items reducer

--- src/app/_reducers/items.reducers.ts
+++ src/app/_reducers/items.reducers.ts
@@ -3,6 +3,18 @@ import { Action } from '@ngrx/store';
 
 export function itemsReducer(state: any = [], action: Action) {
   switch (action.type) {
+    case 'ADD_ITEMS':
+      return action.payload;
+    case 'CREATE_ITEM':
+      return [...state, action.payload];
+    case 'UPDATE_ITEM':
+      return state.map(item => {
+        return item.id === action.payload.id ? Object.assign({}, item, action.payload) : item;
+      });
+    case 'DELETE_ITEM':
+      return state.filter(item => {
+        return item.id !== action.payload.id;
+      });
     default:
       return state;
   }
@@ -11,6 +23,8 @@ export function itemsReducer(state: any = [], action: Action) {
 // The "selectedItem" reducer handles the currently selected items
 export function selectedItemReducer(state: any = null, action: Action) {
   switch (action.type) {
+    case 'SELECT_ITEM':
+      return action.payload;
     default:
       return state;
   }
